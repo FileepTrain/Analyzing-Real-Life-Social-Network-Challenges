253729adafc5a7c29f2c420c663434a9884e55fb
A-Fernandez12 <fernandezadrienne9@gmail.com>
2025-11-24 17:16:43 -0800

    added page rank and centrality

--- graph.py
+++ graph.py
@@ -2,10 +2,18 @@ import networkx as nx
 #import nx_cugraph as nxcg
 import matplotlib.pyplot as plt
 import os 
-
+from time import time
 G = nx.read_edgelist(path='./facebook_combined.txt')
 
 #os.environ['NX_CUGRAPH_AUTOCONFIG'] = "True"
 
-nx.draw(G)
-plt.show()
+start = time()
+#takes about 2 minutes to get page_rank and centrality
+page_rank = nx.pagerank(G)
+centrality = nx.betweenness_centrality(G)
+print(str(abs(time() - start)))
+# nx.draw(G)
+
+# plt.show()
+
+
