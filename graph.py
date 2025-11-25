3bc26ea060da788effb832ea19212221d76ff7d3
A-Fernandez12 <fernandezadrienne9@gmail.com>
2025-11-24 17:19:48 -0800

    added main()

--- graph.py
+++ graph.py
@@ -6,14 +6,16 @@ from time import time
 G = nx.read_edgelist(path='./facebook_combined.txt')
 
 #os.environ['NX_CUGRAPH_AUTOCONFIG'] = "True"
+def main():
+    start = time()
+    #takes about 2 minutes to get page_rank and centrality
+    page_rank = nx.pagerank(G)
+    centrality = nx.betweenness_centrality(G)
+    print(str(abs(time() - start)))
+    # nx.draw(G)
+    # plt.show()
 
-start = time()
-#takes about 2 minutes to get page_rank and centrality
-page_rank = nx.pagerank(G)
-centrality = nx.betweenness_centrality(G)
-print(str(abs(time() - start)))
-# nx.draw(G)
-
-# plt.show()
+if __name__ == '__main__':
+    main()
 
 
